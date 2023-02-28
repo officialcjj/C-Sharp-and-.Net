9db2cae18d15953ddba40db6c36d07de101460a2
officialcjj <officialcjj@gmail.com>
2023-02-27 21:21:01 -0800

    Initial commit

---
+++ First-Site/Project1/Project1/Project1.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Project1.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
